726d98d9819be6d554fab137ca8074ce5b9b17a9
Sunkara Shiva Santosh <104712728+Santosh-sunkara@users.noreply.github.com>
2025-01-31 02:36:32 +0530

    Create App.js

---
+++ backend/frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to StayEase: PG Management System</h1>
+      {/* Add components for login, dashboard, PG list, etc. */}
+    </div>
+  );
+}
+
+export default App;
